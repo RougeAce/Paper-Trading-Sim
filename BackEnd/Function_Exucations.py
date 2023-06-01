ca19f06be56be64b24809b047a1bc746cd01a552
Trey Davidson <treydavidson05@icloud.com>
2023-06-01 16:00:36 -0500

    Currently Tracks Stock Value, and can accept buying and selling of stock

--- BackEnd/Function_Exucations.py
+++ BackEnd/Function_Exucations.py
@@ -2,6 +2,7 @@ from BackEnd import dependines
 import csv
 from datetime import datetime
 import os
+from BackEnd import Values
 
 def check_exceptions(ticker):
     if dependines.is_nyse_open() == False:
@@ -10,32 +11,57 @@ def check_exceptions(ticker):
         return 1001
     return 0
 
-def buy_stock(ticker, amount, account):
-    exception_code = check_exceptions(ticker)
-    price = dependines.stock_value(ticker)
-    if exception_code != 0:
-        return exception_code
-    else:
-        date = datetime.now().strftime("%Y-%m-%d")
-        file_path = f"{account}_stocks.csv"
-        if os.path.exists(file_path):
-            add_stock_file("SELLING", ticker, amount, date, price, file_path)
-        else:
-            create_stock_file("SELLING", ticker, amount, date, price, file_path)
-
-
 def sell_stock(ticker, amount, account):
     exception_code = check_exceptions(ticker)
     price = dependines.stock_value(ticker)
+    file_path = f"{account}_stocks.csv"
+    file_exists = os.path.exists(file_path)
+    date = datetime.now().strftime("%Y-%m-%d")
+    holdings = stock_amount_holding(file_path, ticker)
+
+
+
+
     if exception_code != 0:
         return exception_code
-    else:
-        date = datetime.now().strftime("%Y-%m-%d")
-        file_path = f"{account}_stocks.csv"
-        if os.path.exists(file_path):
-            add_stock_file("SELLING", ticker, amount, date, price, file_path)
+    elif file_exists == False:
+        create_stock_file("SELLING", ticker, amount, date, price, file_path)
+        if holdings > 100000:
+            return 4000
+
+    elif file_exists == True:
+        if holdings < amount:
+            return 4000
+
+
+def buy_stock(ticker, amount, account):
+    exception_code = check_exceptions(ticker)
+    price = dependines.stock_value(ticker)
+    file_path = f"{account}_stocks.csv"
+    cash_path = f"{account}_CASH.csv"
+    file_exists = os.path.exists(file_path)
+    date = datetime.now().strftime("%Y-%m-%d")
+
+
+    if exception_code != 0:
+        return exception_code
+
+
+    elif file_exists == False:
+        if amount * price > 100000:
+            create_stock_file("BUYING", ticker, amount, date, price, file_path)
+            create_cash_account(file_path)
         else:
-            create_stock_file("SELLING", ticker, amount, date, price, file_path)
+            return 4000
+
+
+    elif file_exists == True:
+        if amount * price > check_cash(file_path):
+            return 4000
+
+
+
+
 
 
 
@@ -49,22 +75,26 @@ def create_stock_file(action, ticker,amount, date, price, file_path):
 def add_stock_file(action, ticker,amount, date, price, file_path):
     with open(file_path, "a") as f:
         writer = csv.writer(f)
-        writer.writerow(["ACTION", "TICKER", "AMOUNT", "DATE", "PRICE"])
         writer.writerow([action, ticker, amount, date, price])
     return 0
 
+
+
 def calculate_account_value(file_path):
-    total_value = 0
-    with open(file_path, mode='r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            ticker = row['TICKER']
-            amount = int(row['AMOUNT'])
-            value = dependines.stock_value(ticker)
-            total_value += value * amount
-    return total_value
+    return Values.calculate_account_value(file_path)
 
+def stock_amount_holding(file_path, ticker):
+    return Values.calculate_ticker_amount(file_path, ticker)
 
+def create_cash_account(file_path):
+    with open(file_path, "a") as f:
+        writer = csv.writer(f)
+        writer.writerow(["CASH"])
+        writer.writerow(['100000'])
+    return 0
+
+def check_cash(file_path):
+    Values.check_cash_account_cash_balance(file_path)
 
 
 
